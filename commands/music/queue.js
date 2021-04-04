a86d9b99d520a60954843c697671e402a15136c5
hisokaSV <kololi76@gmail.com>
2021-04-04 09:17:31 +0200

    initial commit

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -8,11 +8,11 @@ module.exports = class QueueCommand extends Command {
             aliases: ['q'],
             group: 'music',
             memberName: 'queue',
-            description: "Affiche la file d'attente, tape le numéro de la page spécifée apres (queue 2)",
+            description: "Affiche la file d'attente, tape le numéro de la page spécifée apres (queue 2).",
             args: [
                 {
                     key: 'page',
-                    prompt: "Quelle page veux tu afficher ?",
+                    prompt: 'Quelle page veux tu afficher ?',
                     default: 1,
                     type: 'integer'
                 }
@@ -28,21 +28,24 @@ module.exports = class QueueCommand extends Command {
     async run(message, { page }) {
         const server = message.client.server;
 
-        if (!message.client.voice.connections.first()) {
-            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `-join` pour m'ajouter");
+        if(!message.client.voice.connections.first()) {
+            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `-join` pour m'ajouter")
         }
 
+        /* page--; */
+
         const numberOfItems = 10;
-        const startingItem = (page - 1) * numberOfItems;
-        const queueLength = server.queue.lenght;
+        const startingItem = (page - 1) *  numberOfItems;
+        const queueLength = server.queue.length;
 
         var ItemsPerPage = startingItem + numberOfItems;
         var totalPages = 1;
 
         var embed = new MessageEmbed()
-            .setTitle(`File d'attente pour ${message.author.usernanme}`)
+            .setTitle(`File d'attente pour ${message.author.username}`)
             .setColor("BLUE")
             .addField('En train de jouer : ', server.currentVideo.url);
+         /* page++; */
 
         if (queueLength > 0) {
             var value = "";
@@ -51,21 +54,20 @@ module.exports = class QueueCommand extends Command {
                 totalPages = Math.ceil(queueLength / numberOfItems);
             }
 
-            if (page <= 0 || (page) > totalPages) {
-                return message.say(":x: cette page n'existe pas :x:");
+            if (page < 0 || (page) > totalPages) {
+                return message.say(":x: Cette page n'existe pas :x:");
             }
 
-            if ( (queueLength - startingItem) < numberOfItems ) {
+            if ( (queueLength - startingItem) < numberOfItems ){
                 ItemsPerPage = (queueLength - startingItem) + startingItem;
             }
 
             for (let i = startingItem; i < ItemsPerPage; i++) {
                 const video = server.queue[i];
-                value += "`" + (i + 1) + ".` " + video.url + "\n";
+                value += "`" + (i + 1) + ".`" + video.url + "\n";
             }
-            embed.addField("A venir :", value);
+            embed.addField("A venir : ", value);
         }
-
         embed.setFooter(`Page ${page}/${totalPages}`);
 
         return message.say(embed);
