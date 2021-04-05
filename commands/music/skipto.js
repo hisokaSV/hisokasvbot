2b3c8bf197fa952dfbef08551352ca3d4a26721b
hisokaSV <kololi76@gmail.com>
2021-04-05 06:10:20 +0200

    initial commit

---
+++ commands/music/skipto.js
@@ -0,0 +1,54 @@
+const { Command, CommandoMessage } = require("discord.js-commando");
+const { UserNotInVoiceChannel, BotNotInVoiceChannel } = require('../../strings.json');
+const ytdl = require('ytdl-core-discord');
+
+module.exports = class SkiptoCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'skipto',
+            aliases: ['n'],
+            group: 'music',
+            memberName: 'skipto',
+            description: 'passe a la musique choisie',
+            args: [
+                {
+                    key: 'index',
+                    prompt: "A quelle position de la file veut tu te rendre ?",
+                    type: 'integer'
+                }
+            ]
+        });
+    }
+
+    /**
+     * 
+     * @param {CommandoMessage} message 
+     * @param {String} query
+     */
+    async run(message, { index }) {
+        const voiceChannel = message.member.voice.channel;
+        const server = message.client.server;
+
+        if (!voiceChannel) {
+            return message.say(UserNotInVoiceChannel);
+        }
+
+        if (!message.client.voice.connections.first()) {
+            return message.say(BotNotInVoiceChannel);
+        }
+
+        index--;
+
+        if (!server.queue[index]) {
+            server.currentVideo = {url: "", title: "Rien pour le momment !"};
+            return message.say("ce titre n'a pas été trouvé dans la filed'attente");
+        }        
+
+        server.currentVideo = server.queue[index];
+
+        server.connection.play( await ytdl(server.currentVideo.url, {filter: 'audioonly' }), {type: 'opus' } );
+        server.queue.splice(index, 1);
+
+        return message.say(":fast_forward: Ignoré :thumbsup:");
+    }
+}
