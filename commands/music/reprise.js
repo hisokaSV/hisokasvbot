0b86828b30b833a5bbf46a79d5718c953dca7edc
hisokaSV <kololi76@gmail.com>
2021-04-05 06:03:47 +0200

    initial commit

--- commands/music/reprise.js
+++ commands/music/reprise.js
@@ -1,3 +1,4 @@
+const { UserNotInVoiceChannel,BotNotInVoiceChannel } = require('../../strings.json');
 const { Command, CommandoMessage } = require("discord.js-commando");
 const { StreamDispatcher } = require('discord.js');
 
@@ -8,7 +9,7 @@ module.exports = class ResumeCommand extends Command {
             aliases: ['r'],
             group: 'music',
             memberName: 'reprise',
-            description: 'Reprend la lecture de la musique en pause'
+            description: 'Reprend la lecture de la musique en pause `-reprise` ou `-r`'
         });
     }
 
@@ -24,10 +25,10 @@ module.exports = class ResumeCommand extends Command {
         const dispatcher = message.client.server.dispatcher;
         
         if (!message.member.voice.channel) {
-            return message.say(':x: Tu dois ètre dans un salon vocal pour utiliser cette commande :x:');
+            return message.say(UserNotInVoiceChannel);
         }
         if (!message.client.voice.connections.first()) {
-            return message.say(":x: Je ne suis pas connecté a un salon vocal. Tape `join` pour m'ajouter :x:");
+            return message.say(BotNotInVoiceChannel);
         }
           
         if(dispatcher){
